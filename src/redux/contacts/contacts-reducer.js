1933bedd40e23eec16c78ef7339fd402b335dd97
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-01-30 19:53:50 +0200

    add default contacts collection

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -10,7 +10,11 @@ import { filterChange } from './contacts-actions';
 // set default store values
 const initialState = {
   contacts: {
-    item: [],
+    item: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    ],
     filter: '',
     isLoading: false,
     error: null,
