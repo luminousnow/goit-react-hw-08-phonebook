82f75050e4dc620d0e404124bab262db92e75625
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-01-30 23:06:29 +0200

    впорядковує імпорти, імпортує шрифт Робото

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+import 'modern-normalize/modern-normalize.css';
+import 'fontsource-roboto';
+import './css/common.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import App from './App';
 import { persistor, store } from './redux/store';
-import 'modern-normalize/modern-normalize.css';
-import './css/common.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
