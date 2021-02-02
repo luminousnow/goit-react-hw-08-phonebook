9567f4d34815b9d2d2881c4cbf95ea15fbfd40e5
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-02-03 01:43:21 +0200

    створює файл, додає початкову розмітку

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+  return (
+    <>
+      <h1>Home page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum odio
+        debitis mollitia modi. Quasi facilis tempore quam, quos repudiandae
+        accusamus sequi perferendis dolor fugiat architecto, possimus asperiores
+        voluptate. Dolore odio culpa a quae. Alias deleniti velit quam
+        doloremque officiis commodi!
+      </p>
+    </>
+  );
+};
