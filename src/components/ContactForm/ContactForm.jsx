a2fc4ded354fa0a72d14d9fcddab3e57a752e4d6
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-02-03 01:38:00 +0200

    додає розмітку, стилізує

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,6 +4,38 @@ import { useDispatch, useSelector } from 'react-redux';
 import { createContact } from '../../redux/contacts/contacts-operations';
 import { getAllContacts } from '../../redux/contacts/contacts-selectors';
 
+//
+
+// import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 function ContactForm() {
   // === useState hook === //
   const [name, setName] = useState('');
@@ -53,41 +85,94 @@ function ContactForm() {
     setNumber('');
   };
 
+  //
+
+  const classes = useStyles();
+
   return (
     <>
-      <form onSubmit={onSubmitPress} className={s.form}>
-        <fieldset className={s.fieldset}>
-          <label className={s.label}>
-            <span>name</span>
-            <input
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar variant="rounded" className={classes.rounded}>
+            <ContactPhoneIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            My phonebook
+          </Typography>
+          <form onSubmit={onSubmitPress} className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
               type="text"
+              id="name"
+              label="Contacts Name"
               name="name"
               value={name}
               onChange={handleImputChange}
-              className={s.input}
+              autoFocus
             />
-          </label>
-          <label className={s.label}>
-            <span>number</span>
-            <input
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
               type="tel"
               name="number"
               value={number}
               onChange={handleImputChange}
-              className={s.input}
+              label="phone number"
+              id="number"
             />
-          </label>
-          <button
-            type="submit"
-            className={s.button}
-            disabled={!(name && number)}
-          >
-            <span>add contacts</span>
-          </button>
-        </fieldset>
-      </form>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              add new contact
+            </Button>
+          </form>
+        </div>
+      </Container>
     </>
+    // <>
+    //   <form onSubmit={onSubmitPress} className={s.form}>
+    //     <fieldset className={s.fieldset}>
+    //       <label className={s.label}>
+    //         <span>name</span>
+    //         <input
+    //           type="text"
+    //           name="name"
+    //           value={name}
+    //           onChange={handleImputChange}
+    //           className={s.input}
+    //         />
+    //       </label>
+    //       <label className={s.label}>
+    //         <span>number</span>
+    //         <input
+    //           type="tel"
+    //           name="number"
+    //           value={number}
+    //           onChange={handleImputChange}
+    //           className={s.input}
+    //         />
+    //       </label>
+    //       <button
+    //         type="submit"
+    //         className={s.button}
+    //         disabled={!(name && number)}
+    //       >
+    //         <span>add contacts</span>
+    //       </button>
+    //     </fieldset>
+    //   </form>
+    // </>
   );
 }
 
-export default ContactForm;
+export { ContactForm };
