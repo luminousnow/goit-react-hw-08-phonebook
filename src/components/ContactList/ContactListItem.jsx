339d56e40cff024f5b1e6b107aa47cf34e5e7da8
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-02-03 01:38:40 +0200

    додає код, стилізує

--- src/components/ContactList/ContactListItem.jsx
+++ src/components/ContactList/ContactListItem.jsx
@@ -1,16 +1,48 @@
 import s from './ContactListItem.module.css';
 import React from 'react';
 import { string, func } from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function ContactListItem({ name, number, onDeleteContact }) {
+  const classes = useStyles();
+
   return (
     <li className={s.listItem}>
       <p className={s.text}>
         {name}: {number}
       </p>
-      <button type="button" className={s.button} onClick={onDeleteContact}>
+      <Button
+        type="button"
+        className={s.button}
+        onClick={onDeleteContact}
+        fullWidth
+        variant="contained"
+        color="primary"
+        // className={classes.submit}
+      >
         delete
-      </button>
+      </Button>
     </li>
   );
 }
