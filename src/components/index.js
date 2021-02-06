eb9b4426d20f23306e8d126eb7e1cda3c640ac0e
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-02-06 23:17:52 +0200

    add imports

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,7 @@ export { ContactForm } from './ContactForm/ContactForm';
 export { Filter } from './Filter/Filter';
 export { ContactList } from './ContactList/ContactList';
 export { ContactListItem } from './ContactList/ContactListItem/ContactListItem';
+export { AppBar } from './AppBar/AppBar';
+export { Navigation } from './Navigation/Navigation';
+export { NavAuth } from './Navigation/NavAuth/NavAuth';
+export { UserMenu } from './UserMenu/UserMenu';
