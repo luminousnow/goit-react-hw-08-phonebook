5afe8911c40f26770e536e3f59a7e2eaf96435dc
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-02-07 01:42:30 +0200

    del unusable code

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -10,11 +10,7 @@ import { filterChange } from './contacts-actions';
 // set default store values
 const initialState = {
   contacts: {
-    item: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    ],
+    item: [],
     filter: '',
     isLoading: false,
     error: null,
