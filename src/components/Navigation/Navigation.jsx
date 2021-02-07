5ffdc9436007719ab36480ca0be7083f61bc5ced
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-02-07 11:56:51 +0200

    додає умову для відображення Компоненту Контакти по умові

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,13 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getIsLoggedIn } from '../../redux/auth/authSelectors';
 
 const Navigation = params => {
+  const isLoggedIn = useSelector(getIsLoggedIn);
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
+      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
     </nav>
   );
 };
